123835e2af7cb8e49a9bfc0ab7299212771721f5
Aman Chokshi <achokshi@student.unimelb.edu.au>
2023-11-24 17:13:38 +0800

    Pushing to save my work on gar

--- eor/scripts/plot_CHIPS.sh
+++ eor/scripts/plot_CHIPS.sh
@@ -3,35 +3,39 @@ module load chips/v3.0
 
 PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 
-# ################################################################################
-# # run_DipAmps
-# ################################################################################
-#
+################################################################################
+# run_DipAmps
+################################################################################
+
 # chips_tags=( 
 #     "1088285600_SIM_FEE" 
 #     "1088285600_SIM_DEF" 
 #     "1088285600_SIM_DEF_CAL_DEF" 
 #     "1088285600_SIM_DEF_CAL_FEE" )
-#
+
 # # #Regular 2D PS
 # for tag in ${chips_tags[@]}; do
 #     echo ${tag}
 #     python $PLTSCRIPT \
+#         --plot_type=2D \
 #         --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/CHIPS_outputs/ \
 #         --chips_tag=${tag} \
 #         --polarisation=both \
-#         --plot_type=2D \
+#         --colourscale=negs_are_grey \
 #         --min_power=1e3 --max_power=1e14 \
 #         --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/plots/
 # done
-#
-# # Ratio
+
+# Ratio
 # python $PLTSCRIPT \
 #     --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/CHIPS_outputs/ \
 #     --chips_tag_one=1088285600_SIM_DEF_CAL_FEE \
 #     --chips_tag_two=1088285600_SIM_DEF_CAL_DEF \
+#     --chips_tag_one_label=SIM_DEF_CAL_FEE \
+#     --chips_tag_two_label=SIM_DEF_CAL_DEF \
 #     --polarisation=both \
 #     --plot_type=2D_ratio \
+#     --colourscale=negs_are_grey \
 #     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/plots \
 #     --min_power=1 --max_power=1e2
 #
@@ -40,6 +44,9 @@ PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 #     --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/CHIPS_outputs/ \
 #     --chips_tag_one=1088285600_SIM_FEE \
 #     --chips_tag_two=1088285600_SIM_DEF \
+#     --chips_tag_one_label=SIM_FEE \
+#     --chips_tag_two_label=SIM_DEF \
+#     --colourscale=negs_are_grey \
 #     --polarisation=both \
 #     --plot_type=2D_ratio \
 #     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/plots \
@@ -51,16 +58,30 @@ PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 #     --chips_tag=1088285600_SIM_DEF \
 #     --chips_tag_one=1088285600_SIM_DEF_CAL_DEF \
 #     --chips_tag_two=1088285600_SIM_DEF_CAL_FEE \
+#     --chips_tag_label=SIM_DEF \
+#     --chips_tag_one_label=SIM_DEF_CAL_DEF \
+#     --chips_tag_two_label=SIM_DEF_CAL_FEE \
 #     --polarisation=both \
 #     --plot_type=2D_ratio_diff \
+#     --colourscale=pos_and_negs \
+#     --min_power=1e0 --max_power=1e3 \
 #     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/plots \
-#     --min_power_r=1 --max_power_r=1e4 --min_power_rd=1 --max_power_rd=1e2
-#
-#
-# ################################################################################
-# # run_T36
-# ################################################################################
-#
+    # --min_power_r=1 --max_power_r=1e4 --min_power_rd=1 --max_power_rd=1e2 \
+
+# # 1D
+# python $PLTSCRIPT \
+#     --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/CHIPS_outputs/ \
+#     --chips_tag_one=${chips_tags[3]} \
+#     --chips_tag_two=${chips_tags[2]} \
+#     --polarisation=both \
+#     --plot_type=1D_comp \
+#     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps/plots/
+
+
+################################################################################
+# run_T36
+################################################################################
+
 # chips_tags=(
 #     "1088285600_SIM_T36" 
 #     "1088285600_SIM_T36_CAL_T36" 
@@ -74,6 +95,7 @@ PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 #         --chips_tag=${tag} \
 #         --polarisation=both \
 #         --plot_type=2D \
+#         --colourscale=negs_are_grey \
 #         --min_power=1e3 --max_power=1e14 \
 #         --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_T36/plots/
 # done
@@ -98,8 +120,8 @@ PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 #     --plot_type=2D_ratio_diff \
 #     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_T36/plots \
 #     --min_power_r=1 --max_power_r=1e4 --min_power_rd=1 --max_power_rd=1e2
-#
-#
+
+
 # ################################################################################
 # # run_DipAmps_flat
 # ################################################################################
@@ -183,21 +205,34 @@ PLTSCRIPT=/astro/mwaeor/achokshi/software/chips/scripts/plotchips_all.py
 #     --plot_type=2D_ratio_diff \
 #     --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps_perfect/plots \
 #     --min_power_r=1 --max_power_r=1e4 --min_power_rd=1 --max_power_rd=1e2
+#
+# ################################################################################
+# # run_10
+# ################################################################################
+#
+# chips_tags=( "run_10" "run_100" "run_1000" "run_10000" )
+#
+# # Regular 2D PS
+# for tag in ${chips_tags[@]}; do
+#     echo ${tag}
+#     python $PLTSCRIPT \
+#         --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_n_sources/${tag} \
+#         --chips_tag=${tag} \
+#         --polarisation=both \
+#         --colourscale=negs_are_grey \
+#         --plot_type=2D \
+#         --min_power=1e3 --max_power=1e14 \
+#         --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_n_sources/run_n_sources/
+# done
 
-################################################################################
-# run_10
-################################################################################
 
-chips_tags=( "run_10" "run_100" "run_1000" "run_10000" )
-
-# Regular 2D PS
-for tag in ${chips_tags[@]}; do
-    echo ${tag}
-    python $PLTSCRIPT \
-        --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/${tag} \
-        --chips_tag=${tag} \
-        --polarisation=both \
-        --plot_type=2D \
-        --min_power=1e3 --max_power=1e14 \
-        --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/plots/
-done
+python $PLTSCRIPT \
+    --basedir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_2s_40kHz/ \
+    --chips_tag=run_2s_40kHz \
+    --polarisation=both \
+    --plot_type=2D \
+    --colourscale=negs_are_grey \
+    --min_power=1e3 --max_power=1e14 \
+    --N_chan=768 --chan_width=40e3 \
+    --outputdir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_2s_40kHz/plots/
+    # --colourscale=negs_are_grey \
--- eor/scripts/run_CHIPS.sh
+++ eor/scripts/run_CHIPS.sh
@@ -3,33 +3,62 @@ module load chips/v3.0
 
 obslist=obs_list.txt
 
-# tags=( "1088285600_SIM_FEE" "1088285600_SIM_DEF" "1088285600_SIM_DEF_CAL_DEF" "1088285600_SIM_DEF_CAL_FEE" )
-# tags=( "1088285600_SIM_T36" "1088285600_SIM_T36_CAL_T36" "1088285600_SIM_T36_CAL_FEE" )
-# tags=( "1088285600_SIM_DEF" "1088285600_SIM_DEF_CAL_DEF_flat" "1088285600_SIM_DEF_CAL_FEE_flat" )
+# tags=(  "1088285600_SIM_DEF_CAL_DEF_MOD_SUB_10000"
+#         "1088285600_SIM_DEF_CAL_DEF_MOD_SUB_1000"
+#         "1088285600_SIM_DEF_CAL_DEF_MOD_SUB_100"
+#         "1088285600_SIM_DEF_CAL_DEF_MOD_SUB_10"
+#         "1088285600_SIM_DEF_CAL_DEF_MOD_SUB_1"
+#         "1088285600_SIM_DEF_CAL_FEE_MOD_SUB_10000"
+#         "1088285600_SIM_DEF_CAL_FEE_MOD_SUB_1000"
+#         "1088285600_SIM_DEF_CAL_FEE_MOD_SUB_100"
+#         "1088285600_SIM_DEF_CAL_FEE_MOD_SUB_10"
+#         "1088285600_SIM_DEF_CAL_FEE_MOD_SUB_1" )
 
-# for tag in ${tags[@]}; do
-#     echo $tag
-#     run_CHIPS.py --cluster=garrawarla \
-#         --obs_list=${obslist} \
-#         --obs_range=0,1 \
-#         --output_dir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/run_DipAmps_flat/CHIPS_outputs/ \
-#         --output_tag=${tag} \
-#         --single_uvfits=${tag}.uvfits \
-#         --data_dir=/astro/mwaeor/achokshi/hyperdrive/eor/data/1088285600/run_DipAmps_flat \
-#         --band=high --no_krig
-# done
+tags=(  "1088285600_SIM_FEE"
+        "1088285600_SIM_T36_CAL_FEE_MOD_SUB_10000"
+        "1088285600_SIM_T36_CAL_FEE_MOD_SUB_1000"
+        "1088285600_SIM_T36_CAL_FEE_MOD_SUB_100"
+        "1088285600_SIM_T36_CAL_FEE_MOD_SUB_10"
+        "1088285600_SIM_T36_CAL_FEE_MOD_SUB_1"
+        "1088285600_SIM_T36_CAL_FEE"
+        "1088285600_SIM_T36_CAL_T36_MOD_SUB_10000"
+        "1088285600_SIM_T36_CAL_T36_MOD_SUB_1000"
+        "1088285600_SIM_T36_CAL_T36_MOD_SUB_100"
+        "1088285600_SIM_T36_CAL_T36_MOD_SUB_10"
+        "1088285600_SIM_T36_CAL_T36_MOD_SUB_1"
+        "1088285600_SIM_T36_CAL_T36"
+        "1088285600_SIM_T36.uvfits" )
 
-tags=( "run_10" "run_100" "run_1000" "run_10000" )
+
+
+DATA_DIR=/astro/mwaeor/achokshi/hyperdrive/eor/data/1088285600/run_T36_2s_80kHz/
+
 
 for tag in ${tags[@]}; do
     echo $tag
-    mkdir -p ${tag}
-    cp /astro/mwaeor/software/chips/template_CHIPS_outputs/* ${tag}
     run_CHIPS.py --cluster=garrawarla \
         --obs_list=${obslist} \
         --obs_range=0,1 \
-        --output_dir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/${tag} \
+        --output_dir=${DATA_DIR}/CHIPS_outputs/ \
         --output_tag=${tag} \
-        --direct_path_list=direct_path_list.txt \
+        --single_uvfits=${tag}.uvfits \
+        --timeres=2 --freqres=80e3 \
+        --data_dir=${DATA_DIR} \
         --band=high --no_krig
 done
+
+# tags=( "run_2s_40kHz" )
+#
+# for tag in ${tags[@]}; do
+#     echo $tag
+#     mkdir -p ${tag}
+#     cp /astro/mwaeor/software/chips/template_CHIPS_outputs/* ${tag}
+#     run_CHIPS.py --cluster=garrawarla \
+#         --obs_list=${obslist} \
+#         --obs_range=0,1 \
+#         --output_dir=/astro/mwaeor/achokshi/hyperdrive/eor/scripts/${tag} \
+#         --output_tag=${tag} \
+#         --timeres=2 --freqres=40e3 \
+#         --direct_path_list=direct_path_list.txt \
+#         --band=high --no_krig
+# done
